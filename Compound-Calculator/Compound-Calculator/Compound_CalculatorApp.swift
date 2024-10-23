c0c565d53657e79f2ad25d164ded515e466ea336
pan3800 <pan173434@gmail.com>
2024-10-23 19:41:07 +0900

    프로젝트 생성

---
+++ Compound-Calculator/Compound-Calculator/Compound_CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Compound_CalculatorApp.swift
+//  Compound-Calculator
+//
+//  Created by 임채성 on 2024/10/23.
+//
+
+import SwiftUI
+
+@main
+struct Compound_CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
